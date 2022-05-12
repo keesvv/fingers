24283627060e3debbc885c250530533c81b2144f
Kees van Voorthuizen <me@keesvv.nl>
2022-05-12 16:42:07 +0200

    refactor: move typer to its own package

--- main.go
+++ main.go
@@ -2,99 +2,24 @@ package main
 
 import (
 	"bufio"
-	"bytes"
-	"crypto/rand"
-	"errors"
 	"flag"
 	"fmt"
 	"io"
-	"math/big"
 	"os"
-	"time"
-	"unicode"
+
+	"keesvv.nl/fingers/typer"
 )
-
-type Typer struct {
-	buf       *bytes.Buffer
-	bps       uint8
-	precision uint8
-	last      byte
-}
-
-func NewTyper(bps, precision uint8) *Typer {
-	return &Typer{new(bytes.Buffer), bps, precision, 0}
-}
-
-func (t *Typer) typo() bool {
-	if t.precision >= 100 {
-		return false
-	}
-
-	if t.precision == 0 {
-		return true
-	}
-
-	n, err := rand.Int(rand.Reader, big.NewInt(99))
-	if err != nil {
-		panic(err)
-	}
-
-	cmp := n.Cmp(big.NewInt(int64(t.precision)))
-	return cmp == 1 || cmp == 0
-}
-
-func (t *Typer) Write(p []byte) (n int, err error) {
-	var buf []byte
-
-	for _, b := range p {
-		buf = append(buf, b)
-
-		if unicode.In(
-			rune(b), unicode.Letter, unicode.Digit,
-		) && t.typo() {
-			buf = append(buf, []byte{'\b', b}...)
-		}
-	}
-
-	return t.buf.Write(buf)
-}
-
-func (t *Typer) Read(p []byte) (n int, err error) {
-	next, err := t.buf.ReadByte()
-	if errors.Is(err, io.EOF) {
-		return 0, err
-	}
-
-	rNext, rLast := rune(next), rune(t.last)
-	delay := time.Second / time.Duration(t.bps)
-	switch {
-	case rNext == rLast && unicode.IsGraphic(rNext), unicode.IsSpace(rNext):
-		delay /= 2
-	case unicode.IsDigit(rNext):
-		delay *= 2
-	case rNext == '\b':
-		delay *= 3
-	case unicode.IsGraphic(rNext):
-		break
-	default:
-		delay = 0
-	}
-
-	t.last = next
-	time.Sleep(delay)
-	return copy(p, []byte{next}), err
-}
 
 func main() {
 	rate := flag.Uint("r", 10, "average amount of bps (bytes per second)")
 	precision := flag.Uint("p", 90, "precision percentage")
 	flag.Parse()
 
-	typer := NewTyper(uint8(*rate), uint8(*precision))
+	t := typer.NewTyper(uint8(*rate), uint8(*precision))
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for scanner.Scan() {
-		fmt.Fprintln(typer, scanner.Text())
-		io.Copy(os.Stdout, typer)
+		fmt.Fprintln(t, scanner.Text())
+		io.Copy(os.Stdout, t)
 	}
 }
